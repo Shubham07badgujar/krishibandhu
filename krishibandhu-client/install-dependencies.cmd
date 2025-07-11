1df87ee40f42926c17050930772a15f68986b0b0
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-07-11 08:38:45 +0530

    Add LoadingSpinner component and install missing dependencies

---
+++ krishibandhu-client/install-dependencies.cmd
@@ -0,0 +1,7 @@
+# Install missing dependencies
+npm install react-hot-toast @heroicons/react axios
+
+# Alternative if the above doesn't work:
+# npm install react-hot-toast
+# npm install @heroicons/react  
+# npm install axios
